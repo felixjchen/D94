d643678f76d238b668b4d659fc23a3a452010c8f
Felix Chen <felixchen1998@gmail.com>
2021-06-13 20:53:52 -0400

    implemented kvs

--- week6/lab/src/lib.rs
+++ week6/lab/src/lib.rs
@@ -1,13 +1,29 @@
-pub struct KvStore;
+use std::collections::HashMap;
+
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
+
+// Suggested by cargo clippy
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 
 impl KvStore {
-  pub fn new() -> KvStore {
-    KvStore {}
-  }
-  pub fn get(&self, _key: String) -> Option<String> {
-    None
-  }
-  pub fn set(&self, _key: String, _value: String) {}
-
-  pub fn remove(&self, _key: String) {}
+    // Returns a new KvStore instance
+    pub fn new() -> KvStore {
+        let map = HashMap::new();
+        KvStore { map }
+    }
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
+    }
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+    }
 }
