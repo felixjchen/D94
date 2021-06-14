075e79a95645660c2d47852e146b70884c969361
Felix Chen <felixchen1998@gmail.com>
2021-06-13 20:56:14 -0400

    Update lib.rs

--- week6/lab/src/lib.rs
+++ week6/lab/src/lib.rs
@@ -17,13 +17,19 @@ impl KvStore {
         let map = HashMap::new();
         KvStore { map }
     }
+    // Given a String, None if not in map, Some(s) if in map
     pub fn get(&mut self, key: String) -> Option<String> {
+        // Get wants a reference, and returns Some(&s), cloned clones the reference.
         self.map.get(&key).cloned()
     }
+    // Insert, if there exists an old value, replace it
     pub fn set(&mut self, key: String, value: String) {
+        // Insert wants two values
         self.map.insert(key, value);
     }
+    // Remove a key from the map
     pub fn remove(&mut self, key: String) {
+        // Remove wants a reference
         self.map.remove(&key);
     }
 }
