6fe18454982b7990f01864716b063c5d74aed06a
Felix Chen <felixchen1998@gmail.com>
2021-07-03 21:24:16 -0400

    Update naive.py

--- week7/reed_solomon/naive.py
+++ week7/reed_solomon/naive.py
@@ -1,5 +1,7 @@
 from lib import *
 import numpy as np
+import itertools
+
 # https://www.cs.cmu.edu/afs/cs/project/pscico-guyb/realworld/www/slidesF15/ecc4.pdf
 
 verbose = True 
@@ -9,11 +11,13 @@ def dp(*args):
     print(*args)
 
 
-n = 5
+n = 7
 k = 3
 d = n-k+1
 
 t = n-k 
+
+evaluation_points = list(range(n))
 
 print(f"A [{n}, {k}, {d}] reed solomon code")
 print(f"Detects {t} errors")
@@ -30,10 +34,7 @@ def encode(message):
       res += coeff * (x**i)
     return res 
 
-  xs = range(1, n+1)
-  ys = [p(i) for i in xs]
-  dp(ys)
-  return ys
+  return [p(i) for i in evaluation_points]
 
 # Interpolate
 def decode(codeword):
@@ -52,35 +53,41 @@ def decode(codeword):
 
   # Get first k-1 deg poly 
   ys = codeword[:k]
-  xs = range(1,k+1)
+  xs = evaluation_points[:k]
   V = np.vander(np.array(xs), k)
-  dp(V, ys)
+  # dp(V, ys)
   coefficients = np.linalg.solve(V, ys)
   coefficients = np.rint(coefficients)
-  dp(coefficients)
+  # dp(coefficients)
 
-  # Check coefficients
-  xs = range(1,n+1)
-  V = np.vander(np.array(xs), k)
+  # Check coefficients works for all points
+  V = np.vander(np.array(evaluation_points), k)
   if np.allclose(V @ coefficients, codeword):
     print("No errors!")
     return list(coefficients)
 
   # 2. 0 < # errors <= t//2 
+  for errors in itertools.product(range(0,n), repeat=t//2):
+    dp("guessing errors:", set(errors))
+
+    remaining_points = set(evaluation_points) - set(errors)
+    dp(f"Checking points {remaining_points}")
 
   # 3. t//2 < # errors <= t 
   pass
 
 if __name__ == "__main__":
   message = [1,0,0]
+  print(f"Message  {message}")
   assert len(message) == k 
 
   codeword = encode(message)
+  print(f"Codeword {codeword}")
   assert len(codeword) == n 
 
   codeword[1] = -44
+  codeword[4] = -44
   red_print("With error", codeword)
 
   ecc_message = decode(codeword)
-
-  print(ecc_message)
+  print(f"Recovered message  {ecc_message}")
